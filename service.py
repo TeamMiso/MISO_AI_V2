f75e4e3c1610e19faab1c839cc46a01f78598632
hang-1n-there <s22037@gsm.hs.kr>
2024-03-25 20:27:04 +0900

    ultralytics add

--- service.py
+++ service.py
@@ -1,21 +1,30 @@
 # service.py : model serving api
-
+import io
+import json
 import numpy as np
+from PIL import Image
 import torch
 from torchvision import transforms
 import base64
 from io import BytesIO
-import bentoml
 from PIL import Image as PILImage
 import requests
-from bentoml.io import DataFrame, JSON
+import pandas as pd
 
+import bentoml
+from bentoml.io import JSON
+
+class_names = {
+        0: 'BAG', 1: 'BATTERY', 2: 'CAN', 3: 'CELL_PHONE', 4: 'CLOTHES',
+        5: 'GENERAL_TRASH', 6: 'GLASSES', 7: 'LAPTOP', 8: 'LDPE', 9: 'LIGHTER',
+        10: 'MOUSE', 11: 'PAPER', 12: 'PAPER_PACK', 13: 'PET', 14: 'PLASTIC',
+        15: 'SHOES', 16: 'TOOTHBRUSH', 17: 'VINYL'
+    }
 yolov8m_runner = bentoml.pytorch.get("yolov8m_model").to_runner()
 svc = bentoml.Service("yolov8_svc", runners=[yolov8m_runner])
-class_names = {0: 'BAG', 1: 'BATTERY', 2: 'CAN', 3: 'CELL_PHONE', 4: 'CLOTHES', 5: 'GENERAL_TRASH', 6: 'GLASSES', 7: 'LAPTOP', 8: 'LDPE', 9: 'LIGHTER', 10: 'MOUSE', 11: 'PAPER', 12: 'PAPER_PACK', 13: 'PET', 14: 'PLASTIC', 15: 'SHOES', 16: 'TOOTHBRUSH', 17: 'VINYL'}
 
-def preprocess_img(imageURL):
-    image = PILImage.open(BytesIO(imageURL))
+def preprocess_img(imageBytes):
+    image = PILImage.open(BytesIO(imageBytes))
 
     transform = transforms.Compose([
         transforms.Resize((640, 640)),
@@ -25,25 +34,20 @@ def preprocess_img(imageURL):
         
     # 입력 이미지의 데이터 타입을 torch.FloatTensor로 변환
     image = transform(image).float().unsqueeze(0)
+    
+    return image
 
-def post_processing(img_tensor, out):
-    cls_names = []
-    for r in out:
-        for c in r.boxes.cls:
-            cls_names.append(class_names[int(c)])
+@svc.api(input=JSON(), output=JSON())
+def imageInference(imageURL: str) -> list:
+    image_url = imageURL.get('imageURL')
 
-    return cls_names
+    # requests를 사용하여 이미지 가져오기
+    response = requests.get(image_url)
+    image_bytes = response.content
 
-@svc.api(input=FormDataInput(), output=JSON())
-def imageInference(imageURL: str):
-    response = requests.get(imageURL)
-    bytesImage = response.content
+    # 이미지 전처리
+    img_tensor = preprocess_img(image_bytes)
 
-    img_tensor = preprocess_img(bytesImage)
-
-    # image inference
+    # 이미지 추론
     out = yolov8m_runner.run(img_tensor)
-
-    result_class_list = post_processing(img_tensor, out)
-
-    return result_class_list
+    return out
