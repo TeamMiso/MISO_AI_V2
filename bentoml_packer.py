541af586affd525ecff8d964094bf7daf20c3bb9
hang-1n-there <s22037@gsm.hs.kr>
2024-03-27 19:39:57 +0900

    🔥 delete files

--- bentoml_packer.py
+++
@@ -1,6 +0,0 @@
-import bentoml
-from ultralytics import YOLO
-
-model = YOLO("best.pt").model
-model.eval()
-saved_model = bentoml.pytorch.save_model(name='yolov8m_model', model=model, signatures={"__call__": {"batchable": False}})
